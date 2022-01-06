5854c043aa0b0b515f8cdd36c4c4ea26b6b72f42
smbjohnson <smbjohnson98@gmail.com>
2022-01-06 15:57:18 -0700

    Adding in Create Header Function

---
+++ R/CreateHeader.R
@@ -0,0 +1,34 @@
+#' @title CreateHeader
+#'
+#' @description Takes the first row a \code{dataframe} and makes it the header.
+#'
+#' @name CreateHeader
+#'
+#' @details 'CreateHeader()' takes the first row a \code{dataframe} and makes it the header.
+#'
+#' @param x a \code{dataframe}.
+#'
+#' @import data.table
+#'
+#'
+#'
+#' @return 'CreateHeader()' returns a \code{dataframe} where the first row is now the header
+#'
+#' @export
+#'
+#' @examples
+#' df <- data.frame(x = c("First", "Sam"), y = c("Height", 178))
+#' CreateHeader(df)
+
+library(data.table)
+
+CreateHeader <- function(x){
+
+  names(x) <- as.matrix(x[1,])
+
+  x <- x[-1,]
+
+  return(x)
+
+}
+
